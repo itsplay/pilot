7b6dd0084a789cfd57f87d0b9120fb7c511f9bfd
Taylor Hobbs <taylor.hobbs@digibox.com.au>
2013-03-12 21:19:29 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pilot::Application.config.secret_token = '2312db271719906ac2403448e105f1f02f22772754235a7a3ec476b0c0447fabc3ade8c1e0cc69bf84dc0cce2c6af15fc71942f041cb8d720db6689ea441f46d'
